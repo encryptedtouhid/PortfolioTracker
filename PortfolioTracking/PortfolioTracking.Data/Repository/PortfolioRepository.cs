10d7656ea92deeb823cc238e9f030d63eff77917
Khaled Md Tuhidul Hossain <me@tuhidulhossain.com>
2023-06-19 00:18:08 +0800

    Create PortfolioRepository.cs

---
+++ PortfolioTracking/PortfolioTracking.Data/Repository/PortfolioRepository.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using PortfolioTracking.Data.Context;
+using PortfolioTracking.Data.EntityModel;
+using PortfolioTracking.Data.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioTracking.Data.Repository
+{
+    public class PortfolioRepository : IPortfolioRepository
+    {
+        public readonly PTDataContext _dataContext;
+        public PortfolioRepository(PTDataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<IEnumerable<Portfolio>> GetAllPortfoliobyTraderIdAsync(string TraderId)
+        {
+            var query = "SELECT * FROM Portfolio WHERE TraderId = @TraderId";
+            using (var connection = _dataContext.CreateConnection())
+            {
+                connection.Open();
+
+                var parameters = new DynamicParameters();
+                parameters.Add("@TraderId", TraderId, DbType.Int32);
+
+                var portfolios = await connection.QueryAsync<Portfolio>(query, parameters);
+                return portfolios.ToList();
+            }
+        }
+
+
+    }
+}
